7ca0e151ebb4cee190c6cbf22c1b281090fe1fe4
Gaurav Suryawanshi <gauravs123456789@gmail.com>
2020-03-13 11:40:28 +0530

    "Updated Working Enviroment.py wrapper"

--- environment.py
+++ environment.py
@@ -6,10 +6,7 @@ class RubikCube(object):
 	"""docstring for env"""
 
 	def __init__(self):
-		self.cube      = pc.cube()
-
-	def _get_states_(self,cub):
-
+		self.cube = pc.Cube()
 
 	def reset(self,max_scrambles):
 		alg = pc.Formula()
@@ -23,48 +20,26 @@ class RubikCube(object):
 
 		self.cube(random_alg)
 
-		return self.cube
+		return utils.flatten_1d_b(self.cube) #return states
 
 
+	def reward(self, tcube):
+		if utils.perc_solved_cube(self.cube)==1:
+			return 1000,True
 
-	def estimated_position(self):
-        """
-        Get the estimated cubie of solved pair.
-        """
-        corner = {"D":self.cube["D"]["D"]}
-        edge = {}
-        for cubie in (corner, edge):
-            for face in self.pair:
-                cubie.update({face:self.cube[face][face]})
-        return (Corner(**corner), Edge(**edge))
-
-	def get_pair(self):
-        colours = (
-            self.cube[self.pair[0]].colour, 
-            self.cube[self.pair[1]].colour, 
-            self.cube["D"].colour
-            )
-        result_corner = self.cube.children.copy()
-        for c in colours[:2]:
-            result_corner &= self.cube.has_colour(c)
-        result_edge = result_corner & self.cube.select_type("edge")
-        result_corner &= self.cube.has_colour(colours[2])
-        return (list(result_corner)[0], list(result_edge)[0])	
-
-	def reward(self):
-		if self.get_pair()==self.estimated_position:
-			return 1000
-		else 
-			return -1
+		return -1,False
 
 
 	def step(self, action):
-		lookup     = ["R", "L","D","U","B","F","R2", "L2","D2","U2","B2","F2"]
+		lookup     = ["R", "L","D","U","B","F","R''", "L''","D''","U''","B''","F''"] #We are not accounting for half turns
 
-		step_taken = pc.formula(lookup[action])
+		tcube = self.cube.copy()
+
+		step_taken = pc.Formula(lookup[action])
 
 		self.cube(step_taken) 
 
-		rwd = self.reward()
+		rwd,over = self.reward(tcube)
 
-		return self.cube
+		return utils.flatten_1d_b(self.cube),rwd,over
+
